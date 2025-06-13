1fcb59b7375a77034b2f825e63ff5f183305f2bd
khelan sahu <khelansahu9584@gmail.com>
2025-06-13 10:24:59 +0530

    make responsive navbar whith hamburger appears

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,53 +1,103 @@
-import React from "react";
-import { FaFacebookF, FaInstagram, FaPinterestP, FaPhone, FaUser } from "react-icons/fa";
+import React, { useState } from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaPinterestP,
+  FaPhone,
+  FaUser,
+} from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 import { FiShoppingBag, FiSearch } from "react-icons/fi";
-import logo from './assests/logo@2x.png'
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
+import logo from "./assests/logo@2x.png";
 
 export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   return (
     <div className="w-full">
- 
-      <div className="bg-[#E4125D] h-[60px] px-13 text-white flex justify-between items-center px-6 py-2 text-sm">
+      {/* Top Bar */}
+      <div className="bg-[#E4125D] h-[60px] px-6 text-white flex justify-between items-center text-sm">
         <div className="flex items-center gap-5">
           <div className="flex items-center gap-2 hover:text-black cursor-pointer">
             <FaPhone /> Hotline 0123456789
           </div>
-          <div className="flex items-center gap-2 hover:text-black cursor-pointer">
+          <div className="flex items-center gap-2 hover:text-black cursor-pointer hidden sm:flex">
             <MdEmail /> info@talemy.com
           </div>
         </div>
-        <div className="flex items-center gap-5">
+        <div className="hidden sm:flex items-center gap-5">
           <FaFacebookF className="cursor-pointer hover:text-black size-4" />
-          <MdEmail className="cursor-pointer  hover:text-black size-4" />
-          <FaInstagram className="cursor-pointer  hover:text-black size-4" />
-          <FaPinterestP className="cursor-pointer  hover:text-black size-4" />
+          <MdEmail className="cursor-pointer hover:text-black size-4" />
+          <FaInstagram className="cursor-pointer hover:text-black size-4" />
+          <FaPinterestP className="cursor-pointer hover:text-black size-4" />
         </div>
       </div>
 
-
-      <div className="bg-white flex  justify-between h-[80px] items-center px-13 py-4 shadow">
-       
-        <div className="flex items-center gap-2 ">
+      <div className="bg-white flex justify-between h-[80px] items-center px-6 py-4 shadow relative">
+        <div className="flex items-center gap-2">
           <img src={logo} alt="logo" className="h-10" />
         </div>
 
- 
-        <div className="flex gap-10 font-bold text-[14px] items-center">
-          <span className="cursor-pointer hover:text-[#E4125D]">Home </span>
-          <span className="cursor-pointer hover:text-[#E4125D]">Pages </span>
-          <span className="cursor-pointer hover:text-[#E4125D]">Courses </span>
-          <span className="cursor-pointer hover:text-[#E4125D]">Events </span>
-          <span className="cursor-pointer hover:text-[#E4125D]">Shop</span>
-          <span className="cursor-pointer hover:text-[#E4125D]">Elements </span>
-          <span className="cursor-pointer hover:text-[#E4125D]">Blog </span>
+        <div className="hidden md:flex gap-10 font-bold text-[14px] items-center">
+          {[
+            "Home",
+            "Pages",
+            "Courses",
+            "Events",
+            "Shop",
+            "Elements",
+            "Blog",
+          ].map((item) => (
+            <span
+              key={item}
+              className="cursor-pointer hover:text-[#E4125D] transition-colors"
+            >
+              {item}
+            </span>
+          ))}
         </div>
 
         <div className="flex items-center gap-6 text-xl">
           <FiShoppingBag className="cursor-pointer hover:text-[#E4125D]" />
           <FiSearch className="cursor-pointer hover:text-[#E4125D]" />
           <FaUser className="cursor-pointer hover:text-[#E4125D]" />
+
+          <div className="md:hidden">
+            {mobileMenuOpen ? (
+              <HiOutlineX
+                className="cursor-pointer text-2xl"
+                onClick={() => setMobileMenuOpen(false)}
+              />
+            ) : (
+              <HiOutlineMenuAlt3
+                className="cursor-pointer text-2xl"
+                onClick={() => setMobileMenuOpen(true)}
+              />
+            )}
+          </div>
         </div>
+
+        {mobileMenuOpen && (
+          <div className="absolute top-[80px] left-0 w-full bg-white shadow-md flex flex-col px-6 py-4 md:hidden z-50">
+            {[
+              "Home",
+              "Pages",
+              "Courses",
+              "Events",
+              "Shop",
+              "Elements",
+              "Blog",
+            ].map((item) => (
+              <span
+                key={item}
+                className="py-2 border-b cursor-pointer hover:text-[#E4125D]"
+              >
+                {item}
+              </span>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
